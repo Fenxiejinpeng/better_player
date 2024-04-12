3c1bccd71f7cb2327cedf5a52af7114bb0ab1985
liuchuancong <chuancong@wangdou-hbean.com>
2024-04-12 12:39:47 +0800

    fix(playback)

--- android/src/main/kotlin/com/alizda/better_player/PlaybackService.kt
+++ android/src/main/kotlin/com/alizda/better_player/PlaybackService.kt
@@ -1,11 +1,17 @@
 package com.alizda.better_player
 
+import android.os.Bundle
 import androidx.annotation.OptIn
+import androidx.core.app.NotificationCompat
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.session.DefaultMediaNotificationProvider
+import androidx.media3.session.CommandButton
+import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.MediaStyleNotificationHelper
+import com.google.common.collect.ImmutableList
+
 class PlaybackService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
 
@@ -13,10 +19,28 @@ class PlaybackService : MediaSessionService() {
         super.onCreate()
         val player = ExoPlayer.Builder(this).build()
         mediaSession = MediaSession.Builder(this, player).build()
-        setMediaNotificationProvider(DefaultMediaNotificationProvider.Builder(this).build()
-        )
+        setMediaNotificationProvider(object : MediaNotification.Provider{
+            override fun createNotification(
+                mediaSession: MediaSession,
+                customLayout: ImmutableList<CommandButton>,
+                actionFactory: MediaNotification.ActionFactory,
+                onNotificationChangedCallback: MediaNotification.Provider.Callback
+            ): MediaNotification {
+                // This run every time when I press buttons on notification bar:
+                return updateNotification(mediaSession)
+            }
+
+            override fun handleCustomCommand(session: MediaSession, action: String, extras: Bundle): Boolean { return false }
+        })
     }
 
+    @OptIn(UnstableApi::class) private fun updateNotification(session: MediaSession): MediaNotification {
+        val notify = NotificationCompat.Builder(this,"Radio")
+            .setStyle(MediaStyleNotificationHelper.MediaStyle(session))
+            .build()
+
+        return MediaNotification(1, notify)
+    }
     override fun onDestroy() {
         mediaSession?.run {
             player.release()
@@ -25,6 +49,5 @@ class PlaybackService : MediaSessionService() {
         }
         super.onDestroy()
     }
-
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
 }
